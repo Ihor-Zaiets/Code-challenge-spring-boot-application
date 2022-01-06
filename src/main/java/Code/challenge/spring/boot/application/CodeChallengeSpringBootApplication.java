cdf1accd17c323245cece7e7bd3fb1cc13f29dc3
ILMentor <dekstroer8202@gmail.com>
2022-01-06 17:37:53 +0100

    init

---
+++ src/main/java/Code/challenge/spring/boot/application/CodeChallengeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Code.challenge.spring.boot.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Code/challenge/spring/boot/application/CodeChallengeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Code.challenge.spring.boot.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
