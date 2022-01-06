9d143c48ae9c5af5bf66467af1f32a89676a7845
ILMentor <dekstroer8202@gmail.com>
2022-01-06 18:00:03 +0100

    Dodałem repozytoria

---
+++ src/main/java/Code/challenge/spring/boot/application/models/ObiektRepo.java
@@ -0,0 +1,6 @@
+package Code.challenge.spring.boot.application.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObiektRepo extends JpaRepository<Obiekt, Long> {
+}
---
+++ src/main/java/Code/challenge/spring/boot/application/models/RezerwacjaRepo.java
@@ -0,0 +1,6 @@
+package Code.challenge.spring.boot.application.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RezerwacjaRepo extends JpaRepository<Rezerwacja, Long> {
+}
