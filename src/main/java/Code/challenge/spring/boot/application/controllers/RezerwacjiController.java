a369add216d6becf94ce748ed6bae7810a71a104
ILMentor <dekstroer8202@gmail.com>
2022-01-10 13:55:53 +0100

    Stworzyłem controllera
    Jeszcze jest surowy. Powinno się googlować jak pracować z @RestController. Jak otraymywać i oddawać dane.

---
+++ src/main/java/Code/challenge/spring/boot/application/controllers/RezerwacjiController.java
@@ -0,0 +1,23 @@
+package Code.challenge.spring.boot.application.controllers;
+
+import Code.challenge.spring.boot.application.models.Rezerwacja;
+import Code.challenge.spring.boot.application.models.RezerwacjaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rezerwacji")
+public class RezerwacjiController {
+
+    private final RezerwacjaRepo rezerwacjaRepo;
+
+    @Autowired
+    public RezerwacjiController(RezerwacjaRepo rezerwacjaRepo) {
+        this.rezerwacjaRepo = rezerwacjaRepo;
+    }
+
+    @PostMapping
+    public void create(@RequestBody Rezerwacja rezerwacja) {
+        rezerwacjaRepo.save(rezerwacja);
+    }
+}
